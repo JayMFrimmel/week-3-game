130515534a3226e94b0cbe06ce55c99f87552f26
JayMFrimmel <jayfrimmel@yahoo.com>
2016-11-05 22:11:43 -0400

    added code

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//establish variables
+var letterGuess = ["a", "b", "c"];
+var userGuess = prompt("Choose a letter!");
+var userGuessLower = userGuess.toLowerCase();
+var letterCount = 3;
+if(letterGuess.indexOf(userGuessLower) == -1) {
+	else{
+		 
+	}
+}
+var isLetter = true;
